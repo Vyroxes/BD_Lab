774a7f893ae98ed614b4dd8b96fe6bcb99533bc0
Vyroxes <Vyroxes@o2.pl>
2025-04-24 13:11:22 +0200

    3 lab

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://bd-lab-2jh5.onrender.com:5000',
+        target: 'https://bd-lab-2jh5.onrender.com',
         changeOrigin: true,
         secure: false,
       },
